1e001cbb5e5da2bdfc521c588b6350456035c816
JessalynSadili <jessalynsadili11@gmail.com>
2024-11-14 21:20:43 -0800

    1116_Video28_Blade-and-Tailwind-Techniques-for-Your-Laravel-Views

---
+++ pixel-positions/resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,20 @@
+<div class="p-4 bg-white/5 rounded-xl flex">
+    <div> 
+        <img src="http://picsum.photos/seed/{{ rand(0,100000) }}/100/100" alt="" clas="rounded-xl">
+    
+    </div>
+
+    <div class="flex-1 flex flex-col">
+        <a href="#" class="self-start text-sm">Laracasts</a>
+
+        <h3>Video Producer</h3>
+
+        <p>Full Time - From $60,000</p>
+    </div>
+
+    <div>
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+    </div>
+</div>
